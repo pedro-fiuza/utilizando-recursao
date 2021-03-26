8933c42b1d357e21884ce7ee43e3383b542063c6
pedro-fiuza <pdrfiuza@gmail.com>
2021-03-26 02:48:58 -0300

    done

---
+++ lib/list_length.ex
@@ -0,0 +1,12 @@
+defmodule ListLength do
+  def call(list), do: list_length(list, 0)
+
+  defp list_length([], acc) do
+    acc
+  end
+
+  defp list_length([_head | tail], acc) do
+    acc = acc + 1
+    list_length(tail, acc)
+  end
+end
